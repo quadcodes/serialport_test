8017657f3f8234df8583281747e57e8f89722be1
Mandar Shirke <quadcodes@gmail.com>
2015-08-22 19:23:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var SerialPort = require("serialport").SerialPort;
+var serialport = new SerialPort("/dev/cu.usbserial-A9E9H3RJ", {
+	parser: require("serialport").parsers.readline("\n")
+});
+
+serialport.on('open', function(){
+  console.log('serial port opened');
+  serialport.on('data', function(data){
+      console.log(data);
+  });
+});
